d0dacf13a80d3c51235c729d7b3033460ae498c1
SourishCMP <sourishbhattacharyya673@gmail.com>
2026-08-16 14:41:22 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -24,13 +24,12 @@ app.use(rateLimiter);
 
 app.use("/api/notes",notesRoutes);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-    })
-
+    app.get("/{*splat}", (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    });
 }
 
 
